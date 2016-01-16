8a1e77a52574c07642d43ea8e3ea064a23725a9f
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-01-16 17:17:45 +0700

    add recaling

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 import csv
 
+import numpy
+
 
 def load_data(file):
     with open(file, newline='') as csvfile:
@@ -7,8 +9,30 @@ def load_data(file):
 
         result = []
         for row in spamreader:
-            result.append(row)
+            if len(row) > 0:
+                result.append(row)
         return result
+
+def convert_to_number(data):
+    return map(lambda row: map(lambda col: float(col),
+                               row),
+               data)
+
+def rescale(data):
+    dataT = numpy.array(data).transpose()
+
+    def rescale_row(row):
+        maximum = max(row)
+        minimum = min(row)
+
+        return map(lambda col: (col - minimum) / (maximum - minimum),
+                   row)
+
+    dataT = map(rescale_row, dataT)
+
+    dataT = to_list(dataT)
+    data = numpy.array(dataT).transpose()
+    return data
 
 def to_list(data):
     if hasattr(data, '__iter__'):
