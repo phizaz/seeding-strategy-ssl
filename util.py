c6af8097b2a1d7bfde6b6decaa1d9365d625d6c1
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-01-16 22:37:29 +0700

    add prime function

--- util.py
+++ util.py
@@ -13,7 +13,7 @@ def load_data(file):
                 result.append(row)
         return result
 
-def convert_to_number(data):
+def to_number(data):
     return map(lambda row: map(lambda col: float(col),
                                row),
                data)
@@ -34,10 +34,23 @@ def rescale(data):
     data = numpy.array(dataT).transpose()
     return data
 
+
 def to_list(data):
-    if hasattr(data, '__iter__'):
+    if hasattr(data, '__iter__') and not isinstance(data, str):
         return list(map(lambda x: to_list(x),
                         data))
     else:
         return data
 
+def is_prime(n):
+    if n == 2 or n == 3: return True
+    if n < 2 or n%2 == 0: return False
+    if n < 9: return True
+    if n%3 == 0: return False
+    r = int(n**0.5)
+    f = 5
+    while f <= r:
+        if n%f == 0: return False
+        if n%(f+2) == 0: return False
+        f +=6
+    return True
