a325fe565eafa3130807bb24d950966114a6fb7c
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-02-21 15:37:51 +0700

    add agglomerative cluster (easy)

---
+++ agglomerative-cluster-pipe.py
@@ -0,0 +1,11 @@
+import util
+from pipe import Pipe
+from wrapper import agglomerative
+from pipetools import dump, evaluate, load_x
+
+file = './datasets/iris/iris.data'
+
+Pipe()\
+    .x(load_x(file, delimiter=','))\
+    .pipe(agglomerative(n_clusters=3))\
+    .pipe(dump('prediction'))
