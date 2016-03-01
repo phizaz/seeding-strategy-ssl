956de99cf06da7fa719d013fdd918f34a0371eb3
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-03-01 23:40:48 +0700

    add good K generator

---
+++ get_K.py
@@ -0,0 +1,43 @@
+from dataset import *
+
+print('working on iris')
+iris = get_iris()
+print('iris K:', iris.K_for_KNN)
+
+print('working on yeast')
+yeast = get_yeast()
+print('yeast K:', yeast.K_for_KNN)
+
+print('working on letter')
+letter = get_letter()
+print('letter K:', letter.K_for_KNN)
+
+print('working on pendigits')
+pendigits = get_pendigits()
+print('pendigits K:', pendigits.K_for_KNN)
+
+print('working on satimage')
+satimage = get_satimage()
+print('satimage K:', satimage.K_for_KNN)
+
+print('working on banknote')
+banknote = get_banknote()
+print('banknote K:', banknote.K_for_KNN)
+
+print('working on eeg')
+eeg = get_eeg()
+print('eeg K:', eeg.K_for_KNN)
+
+print('working on bankmarket')
+bankmarket = get_bankmarket()
+print('bankmarket K:', bankmarket.K_for_KNN)
+
+print('working on magic')
+magic = get_bankmarket()
+print('magic K:', magic.K_for_KNN)
+
+print('working on spam')
+spam = get_spam()
+print('spam K:', spam.K_for_KNN)
+
+print('all K generated')
